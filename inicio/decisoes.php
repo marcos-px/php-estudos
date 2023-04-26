54b77db0cb63e1885474a71d079b18e036ab26ef
Marcos <marcxstx@gmail.com>
2023-04-26 16:14:47 -0300

    Organizando pastas

--- inicio/decisoes.php
+++ inicio/decisoes.php
@@ -1,14 +1,16 @@
 <?php
 
-$idade = 28;
-$nome = 'Juanito';
+$idade = 29;
+$nome = 'Marcos';
+$numeroDePessoas = 2;
 
 echo "Você só pode entrar se tiver mais do que 18 anos." . PHP_EOL;
 
-if ($idade >= 18 and $nome == 'Marcos') {
+if ($idade >= 18 && $nome == 'Marcos') {
     echo "Olá $nome. Você tem $idade anos e seu nome está na lista. Pode entrar." . PHP_EOL;
-} else if ($idade >= 18 and $nome != 'Marcos') {
+} else if ($idade >= 18 && $nome != 'Marcos') {
     echo "Olá $nome. Você tem $idade anos. Você poderia entrar, mas seu nome não está na lista, por isso não pode entrar." . PHP_EOL;
 } else if ($idade < 18) {
     echo "Você só tem $idade anos. Você não pode entrar." . PHP_EOL;
+    echo PHP_EOL . "Adeus!" . PHP_EOL . "É uma pena que você não pode entrar.";
 }
---
+++ inicio/while.php
@@ -0,0 +1,8 @@
+<?php
+
+$contator = 1;
+
+while ($contator <= 15) {
+    echo "#$contator" . PHP_EOL;
+    $contator = $contator + 1;
+}
