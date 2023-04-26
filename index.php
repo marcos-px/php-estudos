d5efaba136f8f04accfca3b4e743275141b0a082
Marcos <marcxstx@gmail.com>
2023-04-25 22:27:14 -0300

    Avançando e, php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
 echo "Hello World!\n";
+
+
+class Main
+{
+    public function __construct()
+    {
+        echo "Main class\n";
+    }
+
+    /**
+     * Just a test
+     *
+     * @param string $name
+     * @return void
+     */
+    public function teste(string $name):void
+    {}
+}
+
+
+(new Main())->teste("Marcos");
+
